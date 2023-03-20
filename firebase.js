d0ca6f20aebb58dd128a9a131721132f19d193de
Fan-Yang <fan.yang284@gmail.com>
2023-03-19 21:47:08 -0400

    firebase

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAoBuWt3x-9STePwSG_xF1LxJc63qVtcjM",
+  authDomain: "engl-108d-creative-project.firebaseapp.com",
+  projectId: "engl-108d-creative-project",
+  storageBucket: "engl-108d-creative-project.appspot.com",
+  messagingSenderId: "374946709105",
+  appId: "1:374946709105:web:ee159e284729e8a93f25d0"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
